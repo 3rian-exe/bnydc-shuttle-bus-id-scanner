ef63b054b748e22d5a728cebd4f3cb0fe73a9e2f
BrianS <briansternfeld51@gmail.com>
2023-10-13 11:49:14 -0400

    Added test swiftproject

---
+++ shuttle-bus-id-scanner/shuttle-bus-id-scanner/shuttle_bus_id_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  shuttle_bus_id_scannerApp.swift
+//  shuttle-bus-id-scanner
+//
+//  Created by Brian Sternfeld on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct shuttle_bus_id_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
