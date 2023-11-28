ec82fdea99ddbb5d01d95d704c030e87a3e653b3
Brian Sternfeld <bsternfeld@BNYDC.ORG>
2023-11-28 11:16:03 -0500

    adding files to personal branch

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import json
+import string
 import urllib3
 import datetime
 import xmltodict
@@ -10,14 +11,14 @@ urllib3.disable_warnings()
 
 #******************************************************************************
 # load the required S2 credentials.
-# This function opens a JSON file path and returns a tuple containing 
+# This function opens a JSON file path and returns a tuple containing
 # the s2 credentials.
 def load_s2_credentials(json_file_path):
     with open(json_file_path) as json_credentials:
         credentials = json.load(json_credentials)
 
         endpoint_url = credentials['S2_HOSTNAME']
-        
+
         return (endpoint_url,)
 #******************************************************************************
 
@@ -30,41 +31,39 @@ def load_card_formts(json_file_path):
     with open(json_file_path) as json_formats:
         formats = json.load(json_formats)
 
-        proxyclick_format = formats['PROXYCLICK']
-        card_fomats_26_bit = formats['26']
-        
-        return (proxyclick_format, card_fomats_26_bit)
+        return formats
 #******************************************************************************
 
 
-proxyclick_format, card_formats_26_bit = load_card_formts('card_formats.json')
-endpoint_url = load_s2_credentials("s2_credentials.json")[0]
+global credential_formats, endpoint_url
+credential_formats = load_card_formts('credential_formats.json')
+endpoint_url = load_s2_credentials('s2_credentials.json')[0]
 
 
 #******************************************************************************
-# Recieves a scan (string) and returns a tuple containing either the RFID scan 
-# or ProxyClick QR code number formatted as a string in the 0th index and a 
-# boolean value indicating what's in the 0th index. The 1st index contains True 
+# Recieves a scan (string) and returns a tuple containing either the RFID scan
+# or ProxyClick QR code number formatted as a string in the 0th index and a
+# boolean value indicating what's in the 0th index. The 1st index contains True
 # if it's an RFID card scan and False if it's a QR code scan.
-# If the input is invalid it will return None. 
+# If the input is invalid it will return None.
 def validate_input(scan: str) -> Union[Tuple[str, bool], None]:
     # Scan is a ProxyClick QR code.
-    if len(scan) >= proxyclick_format['length'] and not scan.isnumeric():
+    if len(scan) >= credential_formats['PROXYCLICK']['length'] and not all(c in string.hexdigits for c in scan):
         qr_code_hotstamp = ''
-        qr_code_hotstamp = scan[ proxyclick_format['start'] : proxyclick_format['end'] ]
+        qr_code_hotstamp = scan[ credential_formats['PROXYCLICK']['start'] : credential_formats['PROXYCLICK']['end'] ]
 
         # double checking that the QR code number is formatted correctly.
         if qr_code_hotstamp.isnumeric():
             return (qr_code_hotstamp, False)
-        
+
         # Scan is not valid.
         else:
             return None
-    
+
     # Scan is an RFID card scan.
-    elif scan.isnumeric():
+    elif all(c in string.hexdigits for c in scan):
         return (scan, True)
-    
+
     # Scan is not valid.
     else:
         return None
@@ -79,7 +78,7 @@ def validate_input(scan: str) -> Union[Tuple[str, bool], None]:
 # If the post request fails it will return None.
 #******************************************************************************
 def get_person_with_hotstamp(session_id: str, hotstamp: str) -> Union[dict, None]:
-    
+
     body = '''
         <NETBOX-API sessionid="{session_id}">
             <COMMAND name="SearchPersonData" num="1">
@@ -89,60 +88,70 @@ def get_person_with_hotstamp(session_id: str, hotstamp: str) -> Union[dict, None
             </COMMAND>
         </NETBOX-API>
         '''.format(session_id=session_id, hotstamp=hotstamp)
-    
+
     try:
         response = post(endpoint_url, body, verify=False)
         response = response.content.decode("utf8")
         response = xmltodict.parse(response)
         response = response['NETBOX']['RESPONSE']
-                
+
         if response['CODE'] != 'SUCCESS':
             return None
-        else: 
+        else:
             return response['DETAILS']['PEOPLE']['PERSON']
     except:
         return None
 #******************************************************************************
 
 
-# Note!
-# The following function is only defined for the wiegand 26 bit RFID card and
-# tested on a select few. 
-
-# Todo Todo Todo Todo Todo Todo Todo Todo Todo Todo Todo Todo Todo Todo Todo 
 #******************************************************************************
-# Recieves an RFID scan in the form of a decimal number as a string and 
+# Recieves an RFID scan in the form of a hexadecimal number as a string and
 # extracts the card format and hotstamp.
-# It returns the card format and hotstamp both as a string and both in a tuple.
+# It returns the card format and hotstamp both as a string in a tuple.
 def extract_card_format_hotstamp(scan: str) -> Union[Tuple[str, str], None]:
 
+    # If the scan was valid.
     if scan:
-        if len(scan) == 8:
-            fc = scan[0:3]
-            hotstamp = scan[3:]
+        # The scan will contain a hex number where the first two digits
+        # represent the cards bit length. This will be campared to a
+        # dictionary where the key is a string of the length. Hence str().
+        bit_length = str( int( scan[0:2], 16 ))
+        # Convert the scan to binary and remove the '0b' in the front for
+        # easier processing.
+        card_bits = bin( int( scan[2:], 16 ))
+        card_bits_in_binary = card_bits[2:]
 
-            fc = str(int(fc))
-            card_formats = card_formats_26_bit
+        # Loop through the credential formats to find the matching bit length.
+        for length, formats in credential_formats.items():
 
-            return (card_formats[fc], hotstamp)
-        else:
-            return (None, None)
-    else:
-        return (None, None)       
+            if length == bit_length:
+
+                for format in formats:
+                    format_facility_code = formats[format]['FACILITY_CODE']
+                    card_facility_code = card_bits_in_binary[ formats[format]['FC_START'] : formats[format]['FC_END'] ]
+                    if card_facility_code != '':
+                        card_facility_code = int(card_facility_code, 2)
+
+                    if card_facility_code == format_facility_code:
+                        encodedd_num = card_bits_in_binary[ formats[format]['ENCODED_NUM_START'] : formats[format]['ENCODED_NUM_END'] ]
+                        encodedd_num = int(encodedd_num, 2)
+
+                        return (format, encodedd_num)
+    return (None, None)
 #******************************************************************************
 
-            
+
 #******************************************************************************
-# This function recieves a dictionary of a passengers data obtained from 
+# This function recieves a dictionary of a passengers data obtained from
 # an API call. It extracts all the required data and returns it in a Passenger
-# object.               
+# object.
 def get_passenger_details(passenger_data: dict, portal_id: str) -> Passenger:
     # Get the current date and time to compare with.
-    current_date_time = datetime.datetime.now() 
+    current_date_time = datetime.datetime.now()
 
     # Get the passengers name.
     passenger_name = passenger_data['FIRSTNAME'] + " " + passenger_data['LASTNAME']
-    
+
     # Get the passengers ID card (or visitor pass) activation date.
     passenger_activation_date = datetime.datetime.strptime(passenger_data['ACTDATE'], "%Y-%m-%d %H:%M:%S")
 
@@ -168,51 +177,51 @@ def get_passenger_details(passenger_data: dict, portal_id: str) -> Passenger:
 
 
 #******************************************************************************
-# Recieves either an RFID card number from the scanner or the encoded/hotstamp 
+# Recieves either an RFID card number from the scanner or the encoded/hotstamp
 # from a ProxyClick QR code scan. Both as a string. Both in a tuple.
 # The 0th index is the RFID card scan or the QR code scan formatted.
-# The 1st index is a boolean indicating which one the 0th index is. 
+# The 1st index is a boolean indicating which one the 0th index is.
 # The 1st index is True for an RFID card scan, and False for a QR code scan.
 # It returns an object of type Passenger which has the following attributes:
-#   - access: a boolean indicating whether or not the passenger is authorized 
+#   - access: a boolean indicating whether or not the passenger is authorized
 #           to ride the bus or not.
-#   - date_time: a date object from the datetime module. it contains the date 
+#   - date_time: a date object from the datetime module. it contains the date
 #                and time of the scan.
-#   - name: a string containing the name of the passenger. Contains 'UNKNOWN' 
+#   - name: a string containing the name of the passenger. Contains 'UNKNOWN'
 #           if the scan was invalid or not found.
-#   - access_portal: a string identifying the bus portal where the passenger scanned 
+#   - access_portal: a string identifying the bus portal where the passenger scanned
 #             thier credentials.
 def get_eligibility(session_id: str, portal_id: str, input_scan: Union[Tuple[str, bool], None]) -> Passenger:
-
     # If input_scan isn't None, then it was a valid scan.
     if input_scan:
         # If the scan is a Proxyclick QR code.
         if input_scan[1] == False:
             # Recieves a dictionary of the visitors data.
             visitor = get_person_with_hotstamp(session_id, input_scan[0])
-            
+
             if visitor:
                 return get_passenger_details(visitor, portal_id)
             # If it failed to retrieve the person data.
             else:
                 return Passenger(False, 'UNKNOWN', portal_id, datetime.datetime.now())
-            
+
         # If the scan is an RFID scan.
         elif input_scan[1] == True:
-
-            # Get the card format, and the hotstamp. 
+            # Get the card format, and the hotstamp.
             card_format, hotstamp = extract_card_format_hotstamp(input_scan[0])
 
             if card_format != None and hotstamp != None:
                 hotstamp = str(int(hotstamp))
                 employee_data = get_person_with_hotstamp(session_id, hotstamp)
-
                 # If there's only one person.
                 if type(employee_data) == dict:
-                    return get_passenger_details(employee_data, portal_id)
-                
+                    if employee_data['ACCESSCARDS']['ACCESSCARD']['CARDFORMAT'] == card_format:
+                        return get_passenger_details(employee_data, portal_id)
+                    else:
+                        return Passenger(False, 'UNKNOWN', portal_id, datetime.datetime.now())
+
                 # If there're multiple PERSONs with that hotstamp.
-                elif type(employee_data) == list:    
+                elif type(employee_data) == list:
                     for employee in employee_data:
                         access_cards = employee['ACCESSCARDS']
 
@@ -224,17 +233,19 @@ def get_eligibility(session_id: str, portal_id: str, input_scan: Union[Tuple[str
                                 # If this employees card format matches return.
                                 if employee_card_format == card_format:
                                     return get_passenger_details(employee, portal_id)
-                
-                        # This employee has only one card.                                    
+
+                        # This employee has only one card.
                         elif type(access_cards['ACCESSCARD']) == dict:
                             if access_cards['ACCESSCARD']['CARDFORMAT'] == card_format:
                                 return get_passenger_details(employee, portal_id)
 
                     # A card format match could not be found.
                     return Passenger(False, 'UNKNOWN', portal_id, datetime.datetime.now())
+                else:
+                    return Passenger(False, 'UNKNOWN', portal_id, datetime.datetime.now())
             else:
                 return Passenger(False, 'UNKNOWN', portal_id, datetime.datetime.now())
-            
+
         # Unknown error occured.
         else:
             return Passenger(False, 'UNKNOWN', portal_id, datetime.datetime.now())
@@ -248,17 +259,17 @@ def get_eligibility(session_id: str, portal_id: str, input_scan: Union[Tuple[str
 #******************************************************************************
 # This function recieves a Passenger object containing the necessary details
 # to be inserted into the s2 activity feed. The details are concatenated into
-# a sting which is then inserted into the activity feed as a user-defined 
+# a sting which is then inserted into the activity feed as a user-defined
 # activity text.
 def insert_activity(session_id: str, passenger: Passenger) -> str:
-    
+
     if passenger.access:
         activity = "Access granted for " + passenger.name + " at " + passenger.access_portal
     else:
         activity = "Access denied for " + passenger.name + " at " + passenger.access_portal
 
     body = '''
-            <NETBOX-API sessionid="{session_id}"> 
+            <NETBOX-API sessionid="{session_id}">
                 <COMMAND name="InsertActivity" num="1">
                     <PARAMS>
                         <ACTIVITYTYPE>USERACTIVITY</ACTIVITYTYPE>
---
+++ scan.py
@@ -0,0 +1,3 @@
+class Scan():
+    def __init__(self, session_id, endpoint_url, ) -> None:
+        pass
